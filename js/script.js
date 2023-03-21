c0dbfbbf2659f9e47940fda2482f8cec0b8409b7
RIF777 <59490850+RIF777@users.noreply.github.com>
2023-03-21 09:54:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
+
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu_item'),
     hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
-        menu.classList.toggle('menu_active');
         hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
-            menu.classList.toggle('menu_active');
             hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
         })
     })
 })
+script.js
+Displaying script.js.
